5ff069245abebd9deb7e682b3670387641175cc6
ZirconCode <gruenings@gmail.com>
2014-08-24 11:56:16 +0900

    best physicsgit add .! + didn't get anymore time yesterday 1h today so far, saddest LD ever

--- src/Player.java
+++ src/Player.java
@@ -11,9 +11,10 @@ public class Player extends Element {
 	private double speedCap = 2.5;
 	private double edgeModifier = 2.5;
 	
-	// TODO earths weapon; a moon on a rope like a wrecking ball
+	// TODO export moon to separate class, make recursive
 	private double mX, mY;
 	private double mXVel, mYVel;
+	private double damper = 0.001;
 	
 	
 	public void render(Graphics g)
@@ -22,7 +23,7 @@ public class Player extends Element {
 		g.drawOval((int)x, (int)y, 50, 50);
 		
 		g.setColor(Color.blue);
-		g.drawOval((int)mXVel, (int)mYVel, 25, 25);
+		g.drawOval((int)mX, (int)mY, 25, 25);
 		
 		g.setColor(Color.white);
 		g.drawLine((int)x,(int)y,(int)mX,(int)mY);
@@ -33,6 +34,11 @@ public class Player extends Element {
 		move(s);
 		
 		System.out.println(xVel);
+	}
+	
+	private double distance(double x1,double y1,double x2,double y2)
+	{
+		return Math.pow(Math.pow(x1-x2, 2)+Math.pow(y1-y2, 2), 0.5);
 	}
 	
 	private void move(GameState s)
@@ -57,8 +63,15 @@ public class Player extends Element {
 		if(y < 0) yVel += ySpeed*edgeModifier;
 		if(y > s.height) yVel -= ySpeed*edgeModifier;
 		
-		// TODO !!! moon "rubber band" "physics"
-		// TODO !!! Gotta go to work now =(
+		// moon "rubber band" "physics"
+		double d = distance(x,y,mX,mY);
+		if(x > mX) mXVel += damper*d;
+		if(x < mX) mXVel -= damper*d;
+		if(y > mY) mYVel += damper*d;
+		if(y < mY) mYVel -= damper*d;
+		// moon momentum
+		mX += mXVel;
+		mY += mYVel;
 		
 		// speed caps
 		xVel = Math.min(Math.max(xVel, -speedCap), speedCap);
